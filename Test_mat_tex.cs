04e50f4246dd8253d3ab64b77d5b64514a4878b0
Chisenon <kamiru.darkness.contractor@gmail.com>
2024-06-28 13:59:25 +0900

    準備完了

--- Test_mat_tex.cs
+++ Test_mat_tex.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using UnityEditor.Animations;
+using VRC.SDK3.Avatars.Components;
 
 public partial class Test_mat_tex : EditorWindow
 {
@@ -9,13 +11,14 @@ public partial class Test_mat_tex : EditorWindow
     private Dictionary<Material, HashSet<Texture>> materialTextures = new Dictionary<Material, HashSet<Texture>>();
     private Vector2 scrollPosition;
     private bool isFoldoutMaterials = true;
+    private Dictionary<AnimatorController, List<string>> animatorUsage = new Dictionary<AnimatorController, List<string>>();
+    private Dictionary<AnimatorController, Dictionary<string, HashSet<AnimationClip>>> animatorClips = new Dictionary<AnimatorController, Dictionary<string, HashSet<AnimationClip>>>();
     private bool isFoldoutAnimator = true;
 
     [MenuItem("CHISENOTE/Test_window_MatTex")]
     private static void ShowWindow()
     {
-        var window = GetWindow<Test_mat_tex>("UIElements");
-        window.titleContent = new GUIContent("Cerberus");
+        var window = GetWindow<Test_mat_tex>("Cerberus");
         window.Show();
     }
 
@@ -25,44 +28,112 @@ public partial class Test_mat_tex : EditorWindow
 
         if (GUILayout.Button("Check Prefab"))
         {
-            materialUsage.Clear();
-            materialTextures.Clear();
+            ClearDictionaries();
+            HashSet<AnimationClip> uniqueClips = new HashSet<AnimationClip>();
 
             if (selectedPrefab != null)
             {
                 Debug.Log("Selected object: " + selectedPrefab.name);
-
-                Renderer[] renderers = selectedPrefab.GetComponentsInChildren<Renderer>();
-                foreach (Renderer renderer in renderers)
-                {
-                    foreach (Material material in renderer.sharedMaterials)
-                    {
-                        if (!materialUsage.ContainsKey(material))
-                        {
-                            materialUsage[material] = new List<GameObject>();
-                            materialTextures[material] = new HashSet<Texture>();
-                        }
-                        materialUsage[material].Add(renderer.gameObject);
-
-                        foreach (var property in material.GetTexturePropertyNameIDs())
-                        {
-                            Texture texture = material.GetTexture(property);
-                            if (texture != null)
-                            {
-                                materialTextures[material].Add(texture);
-                            }
-                        }
-                    }
-                }
-                Debug.Log("Number of unique materials: " + materialUsage.Count);
+                ProcessRenderers();
+                ProcessAvatarDescriptor(uniqueClips);
+                // Debug.Log("Number of unique materials: " + materialUsage.Count);
+                // Debug.Log("Number of unique Textures: " + materialTextures.Count);
+                // Debug.Log("Number of unique Animator Controllers: " + animatorUsage.Count);
+                // Debug.Log("Number of unique Animation Clips: " + uniqueClips.Count);
             }
         }
 
-        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-        GUILayout.BeginHorizontal(); // 横並びの始まり
+        DisplayUI();
+    }
 
-        // Materials and Textures フォルダ
-        EditorGUILayout.BeginVertical(GUILayout.Width(position.width / 2 - 4)); // 左側の領域
+    private void ClearDictionaries()
+    {
+        materialUsage.Clear();
+        materialTextures.Clear();
+        animatorUsage.Clear();
+        animatorClips.Clear();
+    }
+
+    private void ProcessRenderers()
+    {
+        Renderer[] renderers = selectedPrefab.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            foreach (Material material in renderer.sharedMaterials)
+            {
+                if (!materialUsage.ContainsKey(material))
+                {
+                    materialUsage[material] = new List<GameObject>();
+                    materialTextures[material] = new HashSet<Texture>();
+                }
+                materialUsage[material].Add(renderer.gameObject);
+                AddMaterialTextures(material);
+            }
+        }
+    }
+
+    private void AddMaterialTextures(Material material)
+    {
+        foreach (var property in material.GetTexturePropertyNameIDs())
+        {
+            Texture texture = material.GetTexture(property);
+            if (texture != null)
+            {
+                materialTextures[material].Add(texture);
+            }
+        }
+    }
+
+    private void ProcessAvatarDescriptor(HashSet<AnimationClip> uniqueClips)
+    {
+        VRCAvatarDescriptor avatarDescriptor = selectedPrefab.GetComponent<VRCAvatarDescriptor>();
+        if (avatarDescriptor != null)
+        {
+            foreach (var layer in avatarDescriptor.baseAnimationLayers)
+            {
+                if (layer.animatorController is AnimatorController ac)
+                {
+                    if (!animatorUsage.ContainsKey(ac))
+                    {
+                        animatorUsage[ac] = new List<string>();
+                        animatorClips[ac] = new Dictionary<string, HashSet<AnimationClip>>();
+                    }
+                    animatorUsage[ac].Add(layer.type.ToString());
+                    foreach (var acl in ac.layers)
+                    {
+                        if (!animatorClips[ac].ContainsKey(acl.stateMachine.name))
+                        {
+                            animatorClips[ac][acl.stateMachine.name] = new HashSet<AnimationClip>();
+                        }
+                        ExploreStateMachine(acl.stateMachine, uniqueClips, animatorClips[ac][acl.stateMachine.name]);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("VRCAvatarDescriptor component not found");
+        }
+    }
+
+    private void DisplayUI()
+    {
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+        GUILayout.BeginHorizontal();
+
+        float scrollbarWidth = 15.0f;  // スクロールバーの幅を定義
+        float halfWidth = (position.width - scrollbarWidth) / 2 - 4;  // スクロールバーの幅を引いたサイズ
+
+        DisplayMaterialsAndTextures(halfWidth);
+        DisplayAnimatorsAndAnimations(halfWidth);
+
+        GUILayout.EndHorizontal();
+        EditorGUILayout.EndScrollView();
+    }
+
+    private void DisplayMaterialsAndTextures(float width)
+    {
+        EditorGUILayout.BeginVertical(GUILayout.Width(width));
         isFoldoutMaterials = EditorGUILayout.Foldout(isFoldoutMaterials, "Materials and Textures");
         if (isFoldoutMaterials)
         {
@@ -80,17 +151,73 @@ public partial class Test_mat_tex : EditorWindow
             }
         }
         EditorGUILayout.EndVertical();
+    }
 
-        // Animator and Animation フォルダ
-        EditorGUILayout.BeginVertical(GUILayout.Width(position.width / 2 - 4)); // 右側の領域
+    private void DisplayAnimatorsAndAnimations(float width)
+    {
+        EditorGUILayout.BeginVertical(GUILayout.Width(width));
         isFoldoutAnimator = EditorGUILayout.Foldout(isFoldoutAnimator, "Animator and Animation");
         if (isFoldoutAnimator)
         {
-            // Animator and Animation 関連の情報の表示
+            foreach (var animatorController in animatorUsage.Keys)
+            {
+                EditorGUILayout.ObjectField("Animator Controller", animatorController, typeof(AnimatorController), false);
+                foreach (var layerName in animatorUsage[animatorController])
+                {
+                    EditorGUILayout.LabelField("Used in layer: " + layerName);
+                }
+                foreach (var entry in animatorClips[animatorController])
+                {
+                    foreach (var clip in entry.Value)
+                    {
+                        if (!clip.name.Contains("proxy"))
+                        {
+                            EditorGUILayout.BeginHorizontal();
+                            EditorGUILayout.LabelField(clip.name, GUILayout.Width(250));
+                            EditorGUILayout.ObjectField(clip, typeof(AnimationClip), false);
+                            EditorGUILayout.EndHorizontal();
+                        }
+                    }
+                }
+            }
         }
         EditorGUILayout.EndVertical();
+    }
 
-        GUILayout.EndHorizontal(); // 横並びの終わり
-        EditorGUILayout.EndScrollView();
+    private void ExploreStateMachine(AnimatorStateMachine stateMachine, HashSet<AnimationClip> uniqueClips, HashSet<AnimationClip> controllerClips)
+    {
+        foreach (var state in stateMachine.states)
+        {
+            if (state.state.motion is AnimationClip clip && !clip.name.Contains("proxy"))
+            {
+                uniqueClips.Add(clip);
+                controllerClips.Add(clip);
+            }
+            else if (state.state.motion is BlendTree blendTree)
+            {
+                ExploreBlendTree(blendTree, uniqueClips, controllerClips);
+            }
+        }
+
+        foreach (var subStateMachine in stateMachine.stateMachines)
+        {
+            ExploreStateMachine(subStateMachine.stateMachine, uniqueClips, controllerClips);
+        }
+    }
+
+    private void ExploreBlendTree(BlendTree blendTree, HashSet<AnimationClip> uniqueClips, HashSet<AnimationClip> controllerClips)
+    {
+        foreach (var child in blendTree.children)
+        {
+            if (child.motion is AnimationClip clip && !clip.name.Contains("proxy"))
+            {
+                uniqueClips.Add(clip);
+                controllerClips.Add(clip);
+            }
+            else if (child.motion is BlendTree childBlendTree)
+            {
+                ExploreBlendTree(childBlendTree, uniqueClips, controllerClips);
+            }
+        }
     }
 }
